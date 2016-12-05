9ed8ee5e47b0758e919c39e81c8cec4638457471
Raymond Lee <Raymond@Raymonds-MacBook-Air-2.local>
2016-12-04 19:06:17 -0800

    Finish Simon W2D1

---
+++ W2/W2D1/Simon/skeleton/lib/simon.rb
@@ -0,0 +1,67 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+      system('clear')
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
+    end
+  end
+
+  def require_sequence
+    puts "repeat back the sequence, one by one."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    puts "round success"
+  end
+
+  def game_over_message
+    puts "game over. you made it to #{sequence_length - 1} rounds."
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
