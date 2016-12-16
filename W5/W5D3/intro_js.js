7c258b9271ccdbaee6bf6e8c50f747233f3cedf1
Raymond Lee <raylee0525@gmail.com>
2016-12-15 21:00:35 -0800

    Intro to Javascript. callbacks and closures. W5D3

--- W5/W5D3/intro_js.js
+++ W5/W5D3/intro_js.js
@@ -1,37 +1,40 @@
 function madLib(verb, adj, noun) {
-  console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}`);
+  return console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}!`);
 }
 
-console.log(madLib('make', 'best', 'guac'));
+madLib('make', 'best', 'guac');
 
+//
 
 function isSubstring(searchString, subString) {
-  return searchString.includes(subString);
+  return (console.log(searchString.includes(subString)));
 }
 
-console.log(isSubstring("helloworld", "worl"));
+isSubstring('hello', 'ell');
+isSubstring('hello', 'ele');
 
+//
 
 function fizzBuzz(array) {
   let newArr = [];
-
   array.forEach(el => {
     if ((el % 3 === 0) ^ (el % 5 === 0)) {
       newArr.push(el);
     }
   });
 
-  return newArr;
+  return console.log(newArr);
 }
 
-console.log(fizzBuzz([1,2,3,4,5,6,7,8,9,10,15,20,30,33]));
+fizzBuzz([1, 2, 3, 4, 5, 6, 10, 15, 30, 22]);
 
+//
 
-function isPrime(n) {
-  if (n < 2) { return false; }
+function isPrime(num) {
+  if (num < 2) { return false; }
 
-  for (var i = 2; i < n; i++) {
-    if (n % i === 0) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
       return false;
     }
   }
@@ -39,30 +42,36 @@ function isPrime(n) {
   return true;
 }
 
-console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(6));
 console.log(isPrime(13));
 
+//
 
 function sumOfNPrimes(n) {
-  let sum = 0;
   let count = 0;
+  let sum = 0;
 
-  for (var i = 2; count < n; i++) {
+  for (var i = 2; count <= n; i++) {
     if (isPrime(i)) {
       sum += i;
-      count ++;
+      count++;
     }
   }
 
   return sum;
 }
 
+console.log(sumOfNPrimes(1));
 console.log(sumOfNPrimes(2));
+console.log(sumOfNPrimes(10));
 console.log(sumOfNPrimes(100));
 
-function allOrNothing(mod, ...num) {
-  for (var i = 0; i < num.length; i++) {
-    if (num[i] % mod !== 0) {
+//
+
+function allOrNothing(mod, ...nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % mod !== 0) {
       return false;
     }
   }
@@ -70,45 +79,67 @@ function allOrNothing(mod, ...num) {
   return true;
 }
 
-console.log(allOrNothing(2, 1, 3, 6));
-console.log(allOrNothing(3, 6, 9, 12));
+console.log(allOrNothing(3, 6, 9, 12, 39));
+console.log(allOrNothing(2, 6, 9, 12, 39));
 
+//
 
-function titleize(names, callback) {
-  let title = names.map(name => `Mx. ${name} Jingleheim Schmidt`);
-  callback(title);
+function titleize(array, printCallback) {
+  let title = array.map(name => {
+    console.log(`Mx. ${name} Jingleheim Schmidt`);
+  });
+
+  printCallback(title);
 }
 
-titleize(['Ray', 'David', 'Chris'], (names) => {
-  names.forEach(name => console.log(name));
+titleize(['Ray', 'Chris', 'David'], (names) => {
+  names.forEach(name => {
+    return name;
+  });
 });
 
+//
 
-function Elephant(name, height, tricks) {
-  this.name = name;
-  this.height = height;
-  this.tricks = tricks;
+class Elephant {
+  constructor(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+  }
+
+  trumpet() {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!'`);
+  }
+
+  grow() {
+    this.height += 12;
+  }
+
+  addTrick(trick) {
+    this.tricks.push(trick);
+  }
+
+  play() {
+    let trickIdx = Math.floor(Math.random() * this.tricks.length);
+    console.log(`${this.name} is ${this.tricks[trickIdx]}`);
+  }
 }
 
-Elephant.prototype.trumpet = function() {
-  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
-};
+let elefante = new Elephant('Jumbo', 23, ['laughing', 'rolling', 'jumping']);
 
-Elephant.prototype.grow = function() {
-  this.height += 12;
-};
+console.log(elefante.name);
+console.log(elefante.height);
+console.log(elefante.tricks);
+console.log(elefante.trumpet());
+console.log(elefante.grow());
+console.log(elefante.height);
+console.log(elefante.addTrick('screaming'));
+console.log(elefante.tricks);
+console.log(elefante.play());
+console.log(elefante.play());
+console.log(elefante.play());
 
-Elephant.prototype.addTrick = function(trick) {
-  this.tricks.push(trick);
-};
-
-Elephant.prototype.play = function() {
-  let trickIdx = Math.floor(Math.random()*this.tricks.length);
-  console.log(`${this.name} is ${this.tricks[trickIdx]}`);
-};
-
-const e = new Elephant('elephant', 12, ['this', 'that', 'something']);
-console.log(e.play());
+//
 
 let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
 let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
@@ -117,9 +148,24 @@ let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doin
 
 let herd = [ellie, charlie, kate, micah];
 
-
 function paradeHelper(elephant) {
   console.log(`${elephant.name} is trotting by!`);
 }
 
-console.log(herd.forEach(paradeHelper));
+console.log(herd.forEach(elephant => { paradeHelper(elephant); }));
+
+//
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs and bacons please";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+}
+
+let bfastOrder = dinerBreakfast();
+bfastOrder("chocolate chip pancakes");
+bfastOrder("grits");
--- W5/W5D3/javapractice.js
+++ W5/W5D3/javapractice.js
@@ -15,3 +15,36 @@ function myForEach(arr, cb) {
     cb(arr[i]);
   }
 }
+
+
+function isPalindrome(string) {
+  function reverse() {
+    return string.split('').reverse().join('');
+  }
+
+  return string === reverse();
+}
+
+// function scheduleGreatMovieReminder(movie) {
+//   // remind in one min
+//   window.setTimeout(function () {
+//     console.log(`Remember to watch: ${movie}`);
+//   }, 60 * 1000);
+//   console.log(`Timer set for ${movie}`);
+// }
+//
+// scheduleGreatMovieReminder("Citizen Kane");
+// scheduleGreatMovieReminder("Touch of Evil");
+// scheduleGreatMovieReminder("The Third Man");
+
+const readline = require('readline');
+const reader = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.question("name?", function(answer) {
+  console.log(`Hello ${answer}!`);
+
+  reader.close();
+});
