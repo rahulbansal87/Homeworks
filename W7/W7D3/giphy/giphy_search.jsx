ea2c08066c131845f6ef0f8266adb3f2307acea2
Raymond Lee <raylee0525@gmail.com>
2017-01-03 23:31:02 -0800

    complete phase 1 redux

--- W7/W7D3/giphy/giphy_search.jsx
+++ W7/W7D3/giphy/giphy_search.jsx
@@ -2,3 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+import { fetchSearchGiphys } from './actions/giphy_actions';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hello</h1>, root);
+
+  window.store = store;
+  window.fetchSearchGiphys = fetchSearchGiphys;
+  // window.receiveSearchGiphys = receiveSearchGiphys;
+});
