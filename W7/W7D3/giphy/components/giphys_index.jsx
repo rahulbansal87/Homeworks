c360ca7fea535efa834d9e97b11cc6e0c0cf3337
Raymond Lee <raylee0525@gmail.com>
2017-01-04 00:04:57 -0800

    complete giphy search

--- W7/W7D3/giphy/components/giphys_index.jsx
+++ W7/W7D3/giphy/components/giphys_index.jsx
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+
+class GiphysIndex extends React.Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          {this.props.giphys.map((giphy, i) => (
+            <li key={i}>
+              <img src={giphy.images.fixed_height.url} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default GiphysIndex;
--- W7/W7D3/giphy/components/giphys_search.jsx
+++ W7/W7D3/giphy/components/giphys_search.jsx
@@ -1,3 +1,46 @@
 import React, { Component } from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerms: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      searchTerms: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let searchTerms = this.state.searchTerms;
+    this.props.fetchSearchGiphys(searchTerms);
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text"
+            value={this.state.searchTerms}
+            onChange={this.handleChange} />
+          <button onClick={this.handleSubmit}>
+            Search Giphy!
+          </button>
+        </form>
+
+        <GiphysIndex giphys={this.props.giphys}/>
+      </div>
+    );
+  }
+}
+
+export default GiphysSearch;
